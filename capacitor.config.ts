a2536e6f75cd062b86e731f9caf8cf8afd002119
jdbjerrekaer <jdbjerrekaer+github@icloud.com>
2026-01-06 21:41:46 +0100

    Update App and Widget Names to "Yet Another Countdown"
    - Changed the app name in capacitor.config.ts, index.html, and Info.plist files to "Yet another Countdown" for consistency.
    - Updated the Countdown Widget display name in its Info.plist to "Yet another Countdown Widget".
    - Modified the short name in manifest.json to "Yet another Countdown" to reflect the new branding.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.jonatanbjerrekaer.countdown",
-  appName: "Countdown",
+  appName: "Yet another Countdown",
   webDir: "dist",
   server: {
     androidScheme: "https",
